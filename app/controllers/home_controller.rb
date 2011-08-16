b300f9c99305b99c49c84a9827b9f28e08ce89af
Bank <admin@proejctvision.org>
2011-08-16 20:02:29 +0700

    Move checking for teams after checking for users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,23 @@
 class HomeController < ApplicationController
 
-#  include ExtJS::Controller
-#  helper ExtJS::Helpers::Store
-#  helper ExtJS::Helpers::Component
-  
+  #  include ExtJS::Controller
+  #  helper ExtJS::Helpers::Store
+  #  helper ExtJS::Helpers::Component
   def index
-    if current_user.teams.length == 0
-      team = Team.find(1)
-      current_user.teams << team
-      current_user.save!
+    if(current_user.nil?)
+      render :action => 'login'
+    else
+      if current_user.teams.length == 0
+        team = Team.find(1)
+        current_user.teams << team
+        current_user.save!
+      end
+    #logger.debug(current_user.id);
     end
-		if(current_user.nil?)
-		  render :action => 'login'
-		else
-		  #logger.debug(current_user.id);
-		end
   end
 
   def login
-    
+
   end
 
 end
