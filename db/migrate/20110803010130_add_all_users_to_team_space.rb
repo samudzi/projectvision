c2508c45b1e1af3221ac6f94a8ab4304f843ee82
fahad <fahad@goodlogics.com>
2011-08-25 01:58:04 +0500

    fix heroku migration

--- db/migrate/20110803010130_add_all_users_to_team_space.rb
+++ db/migrate/20110803010130_add_all_users_to_team_space.rb
@@ -2,8 +2,8 @@ class AddAllUsersToTeamSpace < ActiveRecord::Migration
   def self.up
     remove_column :users, :team_id
     create_table :teams_users, :id => false do |t|
-      t.string :user_id
-      t.string :team_id
+      t.integer :user_id
+      t.integer :team_id
     end
     
     team = Team.find_by_name('Team Space')
