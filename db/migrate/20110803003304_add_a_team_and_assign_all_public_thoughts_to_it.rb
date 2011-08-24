3d0e47f43607c6e885cafc276627f6588352cd56
fahad <fahad@goodlogics.com>
2011-08-25 02:01:37 +0500

    fix heroku migration

--- db/migrate/20110803003304_add_a_team_and_assign_all_public_thoughts_to_it.rb
+++ db/migrate/20110803003304_add_a_team_and_assign_all_public_thoughts_to_it.rb
@@ -1,16 +1,16 @@
 class AddATeamAndAssignAllPublicThoughtsToIt < ActiveRecord::Migration
   def self.up
-    # team = Team.new :name => "Team Space", :status => "public"
-    #     team.save!
-    #     Thought.where(:scope=>"public").each do |t|
-    #       team.thoughts << t
-    #     end
-    #     team.save!
+    team = Team.new :name => "Team Space", :status => "public"
+    team.save!
+    Thought.where(:scope=>"public").each do |t|
+      team.thoughts << t
+    end
+    team.save!
   end
 
   def self.down
-    # team = Team.find_by_name('Team Space')
-    #     team.destroy if team
-    #     Thought.update_all :team_id => nil
+    team = Team.find_by_name('Team Space')
+    team.destroy if team
+    Thought.update_all :team_id => nil
   end
 end
