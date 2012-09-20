ad05ea21320f9c33f8f2c0621581b82c9bd8332f
Hemant Kumar <gethemant@gmail.com>
2012-09-20 12:17:24 +0530

    Fix breaking migration

--- db/migrate/20120409121129_assign_role_to_already_exsting_team.rb
+++ db/migrate/20120409121129_assign_role_to_already_exsting_team.rb
@@ -1,10 +1,12 @@
 class AssignRoleToAlreadyExstingTeam < ActiveRecord::Migration
   def self.up
-    t = Team.first
-    team_roles = t.team_roles
-    team_roles.each do |tr|
-      tr.role = 2
-      tr.save
+    team = Team.first
+    if team && !team.roles.empty?
+      team_roles = team.team_roles
+      team_roles.each do |tr|
+        tr.role = 2
+        tr.save
+      end
     end
   end
 
