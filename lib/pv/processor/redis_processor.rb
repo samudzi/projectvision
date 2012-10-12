8e4dcfb319ee15afe5005f6940cbf76b0f30eb42
Hemant Kumar <gethemant@gmail.com>
2012-10-12 18:28:45 +0530

    tighten the connection logic around redis connection

--- lib/pv/processor/redis_processor.rb
+++ lib/pv/processor/redis_processor.rb
@@ -7,13 +7,32 @@ module Pv
 
       def check_for_messages
         Pv::Processor.debug("Checking redis queue for incoming messages")
-        message = redis_connection.rpop Pv.config.redis_list_name
+        message = fetch_from_redis
         if message
-          json_object = ActiveSupport::JSON.decode(message)
-          Pv::Processor.info("Found a message of type #{json_object['request_type']}")
-          Pv::Processor::Message.parse(json_object)
+          json_object = parse_json_string(message)
+          if json_object
+            Pv::Processor.info("Found a message of type #{json_object['request_type']}")
+            Pv::Processor::Message.parse(json_object)
+          else
+            Pv::Processor.error("Error parsing incoming json message from redis")
+          end
+        else
+          Pv::Processor.error("Error connecting to redis")
         end
       end
+
+      def parse_json_string(message)
+        ActiveSupport::JSON.decode(message)
+      rescue
+        nil
+      end
+
+      def fetch_from_redis
+        redis_connection.rpop Pv.config.redis_list_name
+      rescue
+        nil
+      end
+
     end
 
   end
