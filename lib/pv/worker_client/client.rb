667eda64978673916609a2ea39fc310e04d6ffbf
Hemant Kumar <gethemant@gmail.com>
2012-10-12 18:32:36 +0530

    Handle unhandled exceptions while writing to redis queue

--- lib/pv/worker_client/client.rb
+++ lib/pv/worker_client/client.rb
@@ -4,15 +4,21 @@ module Pv
       def new_imap_request(imap_address)
         return false unless imap_address
         imap_message = Pv::WorkerClient::NewImapMessage.new(imap_address)
-        redis_connection.rpush Pv.config.redis_list_name,imap_message.to_json
+        send_to_redis(imap_message.to_json)
         true
       end
 
       def trigger_poll(imap_address)
         return false unless imap_address
         imap_message = Pv::WorkerClient::PollInbox.new(imap_address)
-        redis_connection.rpush Pv.config.redis_list_name, imap_message.to_json
+        send_to_redis(imap_message.to_json)
         true
+      end
+
+      def send_to_redis(message)
+        redis_connection.rpush Pv.config.redis_list_name, message
+      rescue
+        Rails.logger.fatal("Error writing to redis")
       end
 
       def redis_connection
